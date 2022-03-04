bca1e2ae5165b3e5325721cd5ec4123467e3a96e
allen <jianyi1217@gmail.com>
2022-03-04 16:22:50 +0800

    Scoring 2

---
+++ src/main/java/com/kcy/Scoring.java
@@ -0,0 +1,10 @@
+package com.kcy;
+
+public class Scoring {
+    public static void main(String[] args) {
+        student jack = new student();
+        jack.name = "Jack";
+        jack.english = 70;
+        jack.math = 88;
+    }
+}
